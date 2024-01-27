b78687bd56fc3a1cafc680fdb82be4b950aec1b6
Kristian Engel <kristianengel@gmail.com>
2024-01-27 08:16:13 -0700

    Update dev from WSL

--- 01-Code/main.py
+++ 01-Code/main.py
@@ -1,33 +1,26 @@
 import pygame, sys, time
 from settings import *
  
- 
 class Game:
     def __init__(self):
         
         # general setup
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
-        pygame.display.set_caption('Breakout')
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Mancer')
  
     def run(self):
-        last_time = time.time()
         while True:
-            
-            # delta time
-            dt = time.time() - last_time
-            last_time = time.time()
- 
-            # event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
  
-            # update window
+            self.screen.fill('black')
             pygame.display.update()
+            self.clock.tick(FPS)
  
 if __name__ == '__main__':
     game = Game()
     game.run()
- 
