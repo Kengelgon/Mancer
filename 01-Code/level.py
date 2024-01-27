9f67c2cdd07fc75c9d427ed7e4b55700311ef0c4
Kristian Engel <kristianengel@gmail.com>
2024-01-27 08:38:19 -0700

    Add world map to level

--- 01-Code/level.py
+++ 01-Code/level.py
@@ -1,5 +1,6 @@
 import pygame
 <<<<<<< HEAD
+<<<<<<< HEAD
 from settings import *
 from tile import Tile
 from player import Player
@@ -58,16 +59,26 @@ class YSortCameraGroup(pygame.sprite.Group):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
 =======
+=======
+from settings import *
+>>>>>>> 23740b1 (Add world map to level)
 
 class Level:
     def __init__(self):
         
         # get the display surface
         self.display_surface = pygame.display.get_surface()
-        
+
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
         self.obstacles_sprites = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map
+    
+    def create_map(self):
+        for row in WORLD_MAP:
+            print(row)
 
     def run(self):
         # update and draw the game
