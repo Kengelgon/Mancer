5dbc244715f767becc4fe081ed75d2b11dc0e522
Kristian Engel <kristianengel@gmail.com>
2024-01-30 13:42:27 -0700

    Test push

--- 01-Code/player.py
+++ 01-Code/player.py
@@ -20,8 +20,11 @@ class Player(pygame.sprite.Sprite):
             self.direction.y = 0
 
         if keys[pygame.K_RIGHT]:
-            self.direction.x = -1
-        elif keys[pygame.K_LEFT]:
             self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
         else:
             self.direction.x = 0
+
+    def update(self):
+        self.input
