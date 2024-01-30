39a59eb8068e19223d6858960c79efdcf72a3f54
Kristian Engel <kristianengel@gmail.com>
2024-01-29 20:37:54 -0700

    Add up down left right key inputs

--- 01-Code/player.py
+++ 01-Code/player.py
@@ -8,3 +8,20 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
 
         self.direction = pygame.math.Vector2()
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = -1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
